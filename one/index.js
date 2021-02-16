705677a88a0683c745024cbe0959ecf5a232a71c
Angelo Shema <geoangercola@gmail.com>
2021-02-17 01:21:16 +0200

    Use @mozilla/readability in `one` instead of @reader-app/readability

--- one/index.js
+++ one/index.js
@@ -1,43 +1,54 @@
-var read = require('node-readability');
 var sanitizeHtml = require('sanitize-html');
+var { Readability, isProbablyReaderable, } = require('@mozilla/readability');
+var { JSDOM } = require('jsdom');
 
-var Typography = require('typography');
+let parse = (content, url) => {
+	var doc = new JSDOM(sanitizeHtml(content), {
+	  url
+	});
+	var readable = isProbablyReaderable(doc.window.document)
+	var title = getTitle(doc.window.document);
 
-const typography = new Typography({
-  baseFontSize: '18px',
-  baseLineHeight: 1.666,
-  headerFontFamily: ['Avenir Next', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
-  bodyFontFamily: ['Georgia', 'serif'],
-})
+	let reader = new Readability(doc.window.document);
+	let article = reader.parse();
 
-let template = (text) => {
-	return (
-`<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width,initial-scale=1">
-		<style id="_typo">${typography.toString()}</style>
-	</head>
-	<body>
-		<main>
-			${text}
-		</main>
-	</body>
-</html>
-`
-)
+	article.title = article.title || title;
+	article.readable = readable;
+	return article;
 }
 
-module.exports = function (data) {
-	return new Promise((resolve, reject) => {
-		read(data, function(err, article) {
-	    if (err) {
-	      reject(err);
-	    } else {
-	    	const content = article.content;
-	    	content.title = article.title;
-	      resolve(template(sanitizeHtml(content)));
-	    }
-	  })
-	})
+getTitle = function(document) {
+  var title = _findMetaTitle(document) || document.title
+  if(!title) {
+  	let firstHead = document.getElementsByTagName('h1')[0]
+  	if (firstHead) title = firstHead.textContent;
+  }
+  var betterTitle;
+  var commonSeparatingCharacters = [' | ', ' _ ', ' - ', '«', '»', '—'];
+
+  commonSeparatingCharacters.forEach(function(char) {
+    var tmpArray = title.split(char);
+    if (tmpArray.length > 1) {
+      if (betterTitle) return title;
+      betterTitle = tmpArray[0].trim();
+    }
+  });
+
+  return title;
+};
+
+function _findMetaTitle(document) {
+  var metaTags = document.getElementsByTagName('meta');
+  var tag;
+
+  for(var i = 0; i < metaTags.length; i++) {
+    tag = metaTags[i];
+
+    if(tag.getAttribute('property') === 'og:title' || tag.getAttribute('name') === 'twitter:title'){
+      return tag.getAttribute('content');
+    }
+  }
+  return null;
 }
+
+module.exports = parse
