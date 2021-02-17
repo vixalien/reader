2227072e3b643f4ade968534a09f22d764c07220
Angelo Shema <geoangercola@gmail.com>
2021-02-17 05:24:32 +0200

    one: extract metadata
    - Extract metadata from the page such as title, image, author etc.
    - Excerpt is limited to 170 characters

--- one/index.js
+++ one/index.js
@@ -1,54 +1,38 @@
 var sanitizeHtml = require('sanitize-html');
 var { Readability, isProbablyReaderable, } = require('@mozilla/readability');
 var { JSDOM } = require('jsdom');
+var extract = require('./extract');
 
-let parse = (content, url) => {
-	var doc = new JSDOM(sanitizeHtml(content), {
+let parse = async (content, url) => {
+	var unsanitizedDoc = new JSDOM(content, {
 	  url
 	});
+
+  var doc = new JSDOM(sanitizeHtml(content), {
+    url
+  });
+
+  var extractedData = await extract(unsanitizedDoc, url);
+
 	var readable = isProbablyReaderable(doc.window.document)
-	var title = getTitle(doc.window.document);
 
 	let reader = new Readability(doc.window.document);
 	let article = reader.parse();
 
-	article.title = article.title || title;
 	article.readable = readable;
+
+  // Merge previously extracted data
+  for (let key in extractedData) {
+    article[key] = extractedData[key] || article[key];
+  }
+
+  // Fix excerpt
+  if (article.excerpt) {
+    // Limit the excerpt to 170 characters, then remove the last word (it should be broken)
+    article.excerpt = article.excerpt.substr(0, 170).replace(/ (\S)$/, '')
+  }
+
 	return article;
-}
-
-getTitle = function(document) {
-  var title = _findMetaTitle(document) || document.title
-  if(!title) {
-  	let firstHead = document.getElementsByTagName('h1')[0]
-  	if (firstHead) title = firstHead.textContent;
-  }
-  var betterTitle;
-  var commonSeparatingCharacters = [' | ', ' _ ', ' - ', '«', '»', '—'];
-
-  commonSeparatingCharacters.forEach(function(char) {
-    var tmpArray = title.split(char);
-    if (tmpArray.length > 1) {
-      if (betterTitle) return title;
-      betterTitle = tmpArray[0].trim();
-    }
-  });
-
-  return title;
-};
-
-function _findMetaTitle(document) {
-  var metaTags = document.getElementsByTagName('meta');
-  var tag;
-
-  for(var i = 0; i < metaTags.length; i++) {
-    tag = metaTags[i];
-
-    if(tag.getAttribute('property') === 'og:title' || tag.getAttribute('name') === 'twitter:title'){
-      return tag.getAttribute('content');
-    }
-  }
-  return null;
 }
 
 module.exports = parse
